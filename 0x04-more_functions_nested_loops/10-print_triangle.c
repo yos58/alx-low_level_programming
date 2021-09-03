7b581b5ebdb71d054489a9bdc8bed99d050d1d90
yos58 <ywondimu82@gmail.com>
2021-09-03 16:48:35 +0300

    commit of project 0x04. C corection

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,24 +6,24 @@
  */
 void print_triangle(int size)
 {
-  int hash, index;
+int hash, index;
 
-  if (size > 0)
-    {
-      for (hash = 1; hash <= size; hash++)
-	{
-	  for (index = size - hash; index > 0; index--)
-	    _putchar(' ');
+if (size > 0)
+{
+for (hash = 1; hash <= size; hash++)
+{
+for (index = size - hash; index > 0; index--)
+_putchar(' ');
 
-	  for (index = 0; index < hash; index++)
-	    _putchar('#');
+for (index = 0; index < hash; index++)
+_putchar('#');
 
-	  if (hash == size)
-	    continue;
+if (hash == size)
+continue;
 
-	  _putchar('\n');
-	}
-    }
+_putchar('\n');
+}
+}
 
   _putchar('\n');
 }
