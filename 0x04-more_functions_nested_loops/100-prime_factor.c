ad1a4a20b3eb1901882bb3f8b8d638cb0e18f072
yos58 <ywondimu82@gmail.com>
2021-09-03 16:52:15 +0300

    commit of project 0x04. C corection boness

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -15,7 +15,7 @@ while (div < (prime / 2))
 if ((prime % 2) == 0)
 {
 prime /= 2;
- continue;
+continue;
 }
 
 for (div = 3; div < (prime / 2); div += 2)
@@ -25,7 +25,7 @@ prime /= div;
 }
 }
 
-  printf("%ld\n", prime);
+printf("%ld\n", prime);
 
-  return (0);
+return (0);
 }
