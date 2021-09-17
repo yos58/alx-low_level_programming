395741ccc0db6fc24be5e275fd27612e1909330b
yos58 <ywondimu82@gmail.com>
2021-09-17 20:47:57 +0300

    commit of project 0x0A. C - argc, argv boness

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,28 +1,31 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * main - Prints the multiplication of two args numbers
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: Always zero
+ * main - Program that takes first two integer arguments and prints the product
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if not enough arguments passed in, 0 otherwise
  */
+
 int main(int argc, char *argv[])
 {
-int n1 = 0, n2 = 0;
+int i, j;
 
-if (argc == 3)
-{
-n1 = atoi(argv[1]);
-n2 = atoi(argv[2]);
-printf("%d\n", n1 * n2);
-}
-else
+if (argc == 1 || argc == 2)
 {
 printf("Error\n");
 return (1);
+}
+else
+{
+j = 1;
+
+for (i = 1; i < 3; i++)
+j *= atoi(argv[i]);
+
+printf("%d\n", j);
 }
 
 return (0);
